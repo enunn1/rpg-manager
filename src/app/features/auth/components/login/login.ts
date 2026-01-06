8b690249fc56e6bd8b34bcf7f3d7007d6183f4cb
Ethan Nunn <nunn_ethan@yahoo.com>
2026-01-05 19:04:11 -0600

    Added login and register functionality

--- src/app/features/auth/components/login/login.ts
+++ src/app/features/auth/components/login/login.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../../core/auth/auth.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class LoginComponent {
+    email = '';
+  password = '';
+  error?: string;
 
+  constructor(private auth: AuthService, private router: Router) {}
+
+  submit() {
+    this.auth.login(this.email, this.password).subscribe({
+      next: () => this.router.navigate(['/dashboard']),
+      error: () => (this.error = 'Invalid email or password'),
+    });
+  }
 }
--- src/app/features/auth/components/register/register.ts
+++ src/app/features/auth/components/register/register.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../../core/auth/auth.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
 export class RegisterComponent {
+   email = '';
+  displayName = '';
+  password = '';
+  error?: string;
 
+  constructor(private auth: AuthService, private router: Router) {}
+
+  submit() {
+    this.auth.register(this.email, this.displayName, this.password).subscribe({
+      next: () => this.router.navigate(['/dashboard']),
+      error: () => (this.error = 'Registration failed'),
+    });
+  }
 }
